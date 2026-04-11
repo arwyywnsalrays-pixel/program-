57699325f6a2966c4db7733dcf89f9fb62951334
arwyywnsalrays-pixel <arwyywnsalrays@gmail.com>
2026-04-12 00:15:30 +0200

    Add files via upload

---
+++ ai.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <set>
+#include <tuple>
+
+using namespace std;
+
+// This struct represents one state in the problem
+struct State {
+    int x, y;          // position
+    int fuel;          // fuel amount
+    bool c1, c2, c3, c4; // coins
+
+    // constructor
+    State(int x, int y, int fuel, bool c1, bool c2, bool c3, bool c4) {
+        this->x = x;
+        this->y = y;
+        this->fuel = fuel;
+        this->c1 = c1;
+        this->c2 = c2;
+        this->c3 = c3;
+        this->c4 = c4;
+    }
+
+    // this is needed for set (to avoid repeated states)
+    bool operator<(const State& o) const {
+        return tie(x,y,fuel,c1,c2,c3,c4) < tie(o.x,o.y,o.fuel,o.c1,o.c2,o.c3,o.c4);
+    }
+};
+
